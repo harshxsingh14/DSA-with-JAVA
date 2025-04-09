483c226857e629bba9556ea69e97630481cdd64d
harshxsingh14 <harsh.singh1403@gmail.com>
2025-04-09 22:22:20 +0530

    Java Basics

---
+++ 1. Java_concepts/1.1.Java Basics/FirstProgram.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+
+public class FirstProgram {
+  public static void main(String[] args) {
+    System.out.println("Welcome");
+    System.out.println("to Java Programming");
+    System.out.println("This is my first program");
+  }
+}
