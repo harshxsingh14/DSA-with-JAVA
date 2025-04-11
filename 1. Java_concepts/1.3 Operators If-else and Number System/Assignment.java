bf99b4e8643230fac43c1a829c35c8556182f031
harshxsingh14 <harsh.singh1403@gmail.com>
2025-04-11 19:43:37 +0530

    Assignment

--- 1. Java_concepts/1.3 Operators If-else and Number System/Assignment.java
+++ 1. Java_concepts/1.3 Operators If-else and Number System/Assignment.java
@@ -1,6 +1,6 @@
 public class Assignment {
     public static void main(String[] args) {
-        int myInt = 9;
+        int myInt = 32;
         System.out.println(myInt);
         int newInt = myInt;
         System.out.println(newInt);
