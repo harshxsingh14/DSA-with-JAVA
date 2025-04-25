60a8495b6dda05534fd97acfc56522604b763adf
harshxsingh14 <harsh.singh1403@gmail.com>
2025-04-25 19:31:41 +0530

    Array utility program

---
+++ 2. Arrays/Concept pracrice/ArrayUtility.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+
+class ArrayUtility {
+    public static int[] inputArray() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+}
+
+/* 
+import java.util.Scanner;
+
+public class ArrayUtility {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static int[] inputArray() {
+        System.out.print("Enter size of the array: ");
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter " + size + " sorted integers:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        return arr;
+    }
+
+    public static void displayArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println(); // For newline
+    }
+}
+*/
