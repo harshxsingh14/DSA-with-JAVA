ab4562107514b6ee07f5b8528875cf2a2dae2e21
harshxsingh14 <harsh.singh1403@gmail.com>
2025-04-28 07:26:28 +0530

    Update EqualsAndHashCodeTest.java

--- 1. Java_concepts/1.7/equals/EqualsAndHashCodeTest.java
+++ 1. Java_concepts/1.7/equals/EqualsAndHashCodeTest.java
@@ -2,8 +2,8 @@ package in.kgcoding.equals;
 
 public class EqualsAndHashCodeTest {
     public static void main(String[] args) {
-        Person person1 = new Person("Prashant", 31, "001");
-        Person person2 = new Person("Prashant", 30, "001");
+        Person person1 = new Person("Harsh, 31, "001");
+        Person person2 = new Person("Harsh", 30, "001");
 
         if (person1.equals(person2)) {
             System.out.println("Equals");
