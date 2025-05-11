1dea3d8b7ad6072dfe03a727220b3826892dbb1f
harshxsingh14 <harsh.singh1403@gmail.com>
2025-05-11 11:32:34 +0530

    Create TestingWrapper.java

---
+++ 1. Java_concepts/2.0/src/wrapper/TestingWrapper.java
@@ -0,0 +1,9 @@
+
+public class TestingWrapper {
+    public static void main(String[] args) {
+        Integer first = 55;
+        System.out.println(first);
+        int second = first;
+
+    }
+}
