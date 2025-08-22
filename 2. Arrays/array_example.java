3a2face2a63af278bd01b8ab2e9d87a7d9b85f49
harshxsingh14 <harsh.singh1403@gmail.com>
2025-08-22 19:57:35 +0530

    Array example code

---
+++ 2. Arrays/array_example.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class array_example {
+    public static void main(String[] args) {
+        System.out.println("Enter the numbers\n");
+        Scanner in = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>(5);
+
+        for (int i = 0; i < 5; i++) {
+            list.add(in.nextInt());
+        }
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(list.get(i));
+        }
+
+        System.out.println(list); 
+    }
+}
