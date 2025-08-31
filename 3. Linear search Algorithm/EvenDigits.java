cbb856c85585f1c50fd6f96e0bb81e0f5cccfa64
harshxsingh14 <harsh.singh1403@gmail.com>
2025-09-01 00:03:54 +0530

    Even digits program

--- 3. Linear search Algorithm/EvenDigits.java
+++ 3. Linear search Algorithm/EvenDigits.java
@@ -1,14 +1,15 @@
-
 public class EvenDigits {
     public static void main(String[] args) {
-        int[] nums = {12,345,2,6,7896};
-//        System.out.println(findNumbers(nums));
+        int[] nums = {12, 345, 2, 6, 7896};
 
+        System.out.println(findNumbers(nums));
         System.out.println(digits2(-345678));
+        System.out.println(digits2(0));
     }
+
     static int findNumbers(int[] nums) {
         int count = 0;
-        for(int num : nums) {
+        for (int num : nums) {
             if (even(num)) {
                 count++;
             }
@@ -16,15 +17,8 @@ public class EvenDigits {
         return count;
     }
 
-    // function to check whether a number contains even digits or not
     static boolean even(int num) {
         int numberOfDigits = digits(num);
-        /*
-        if (numberOfDigits % 2 == 0) {
-            return true;
-        }
-        return false;
-         */
         return numberOfDigits % 2 == 0;
     }
 
@@ -32,16 +26,16 @@ public class EvenDigits {
         if (num < 0) {
             num = num * -1;
         }
+        if (num == 0) {
+            return 1;
+        }
         return (int)(Math.log10(num)) + 1;
     }
 
-    // count number of digits in a number
     static int digits(int num) {
-
         if (num < 0) {
             num = num * -1;
         }
-
         if (num == 0) {
             return 1;
         }
@@ -49,11 +43,8 @@ public class EvenDigits {
         int count = 0;
         while (num > 0) {
             count++;
-            num = num / 10; // num /= 10
+            num = num / 10;
         }
-
         return count;
     }
-
-
 }
