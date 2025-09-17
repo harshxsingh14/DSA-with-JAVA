50fe63ff9aa7b7b122ccf8afabc9d653cd04d916
harshxsingh14 <harsh.singh1403@gmail.com>
2025-09-17 23:28:59 +0530

    Dynamic Stack Program

---
+++ 10. Stack And Queues/DynamicStack.java
@@ -0,0 +1,16 @@
+public class DynamicStack extends CustomStack {
+
+    public DynamicStack(int size) {
+        super(size);
+    }
+
+    @Override
+    public boolean push(int item) throws StackException {
+        if (isFull()) {
+            int[] temp = new int[data.length * 2];
+            System.arraycopy(data, 0, temp, 0, data.length);
+            data = temp;
+        }
+        return super.push(item);
+    }
+}
