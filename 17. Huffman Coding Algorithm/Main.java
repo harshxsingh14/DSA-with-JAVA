2f45657bae5918e175102c6ee9ce2a377586c5cf
harshxsingh14 <harsh.singh1403@gmail.com>
2025-09-28 22:53:42 +0530

    Huffman Coding Algorithm

---
+++ 17. Huffman Coding Algorithm/Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) throws Exception{
+    String str = "HarshSingh";
+      HuffmanCoder hf = new HuffmanCoder(str);
+    String a = hf.encode(str);
+    System.out.println(a);
+    String b = hf.decode(a);
+    System.out.println(b);
+
+
+  }
+}
