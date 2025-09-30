2843ee6bcd4301e2739cf8f6ffb45f8a593d5273
harshxsingh14 <harsh.singh1403@gmail.com>
2025-09-30 22:42:53 +0530

    File handling

---
+++ 19. File Handling/Main.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.io.IOException;
+//import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+
+    // create
+    try {
+      File fo = new File("new-file.txt");
+      fo.createNewFile();
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    }
+
+    // write in the file
+    try {
+      FileWriter fw = new FileWriter("file.txt");
+      fw.write("One Piece is the greatest piece of fiction");
+      fw.close();
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    }
+
+    // reading from a file
+    try (BufferedReader br = new BufferedReader(new FileReader("file.txt"))) {
+      while (br.ready()) {
+        System.out.println(br.readLine());
+      }
+    } catch (IOException e) {
+        System.out.println(e.getMessage());      
+    }
+
+    try {
+      File fo = new File("random.txt");
+      fo.createNewFile();
+      if(fo.delete()) {
+        System.out.println(fo.getName());
+      }
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    }
+
+    
+    
+  }
+}
