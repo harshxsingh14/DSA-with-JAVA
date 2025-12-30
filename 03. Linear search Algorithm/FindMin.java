196c304a318d4e07650e13bf593e7ccbe3a832e5
HARSH KUMAR SINGH <147037629+harshxsingh14@users.noreply.github.com>
2025-12-30 23:08:35 +0530

    Update FindMin.java

--- 03. Linear search Algorithm/FindMin.java
+++ 03. Linear search Algorithm/FindMin.java
@@ -1,6 +1,6 @@
 public class FindMin {
     public static void main(String[] args) {
-        int[] arr = {18, 12, 7, 3, 14, 28};
+        int[] arr = {18, 132, 7, 3, 17, 28};
         System.out.println(min(arr));
     }
 
